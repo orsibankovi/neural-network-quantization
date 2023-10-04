c10c2f10432d7426a18aa587ced25d91f42198c6
Orsolya Bankovi <orsolya.bankovi@aimotive.com>
2023-10-04 07:05:28 +0200

    first commit

--- main.py
+++ main.py
@@ -2,24 +2,43 @@ import model_load
 import torch
 import transformers
 import quant_int8
-import nnef_tools
 
 if __name__ == "__main__":
+    #image classification (IMAGENET-1K)
+    efficientnet = transformers.EfficientNetForImageClassification.from_pretrained("google/efficientnet-b7")
+    efficientnet = torch.hub.load('NVIDIA/DeepLearningExamples:torchhub', 'nvidia_efficientnet_b0', pretrained=True)
+    efficientnet.eval()
+    
+    efficientnet_int8 = quant_int8.quantize_model(efficientnet)
+    '''
+    for name, param in efficientnet.named_parameters():
+        print(name)
+        
+    for name, param in efficientnet_int8.named_parameters():
+        print(name)
+    '''
+    #segformer = transformers.SegformerForImageClassification.from_pretrained("nvidia/mit-b0")
+    
     #object detection
+    '''
     yolov5m = model_load.load_models('ultralytics/yolov5', 'yolov5m')
     yolov5m.eval()
-    #yolos_tiny =  transformers.YolosForObjectDetection.from_pretrained('hustvl/yolos-tiny')
-    #yolos_tiny.eval()
-
-    #image classification
-    #efficientnet = transformers.EfficientNetForImageClassification.from_pretrained("google/efficientnet-b7")
-    #segformer = transformers.SegformerForImageClassification.from_pretrained("nvidia/mit-b0")
-    #efficientnet = torch.hub.load('NVIDIA/DeepLearningExamples:torchhub', 'nvidia_efficientnet_b0', pretrained=True)
-    #efficientnet.eval()
-
     quant_yolov5m = quant_int8.torch_quantize(yolov5m)
     dummy_input = torch.randn(1, 3, 640, 640)
     onnx_yolov5m = torch.onnx.export(quant_yolov5m, dummy_input, 'yolov5m.onnx')
     for name, param in quant_yolov5m.named_parameters():
-        print(name, param.size())
+        print(name, param.data)
+        
+    for name, param in yolov5m.named_parameters():
+        print(name, param.data)
+        
+    yolos_tiny =  transformers.YolosForObjectDetection.from_pretrained('hustvl/yolos-tiny')
+    yolos_tiny.eval()
+    
+    for name, param in yolos_tiny.named_parameters():
+        print(name, param.data)
+        
+    for name, param in yolos_tiny.named_parameters():
+        print(name, param.data)
+    '''
     
--- quant_int8.py
+++ quant_int8.py
@@ -1,8 +1,42 @@
 import torch
+from torch import Tensor
 
-def torch_quantize(model, backend='x86'):
-    model.eval()
-    #scripted_model = torch.jit.script(model)
-    model.qconfig = torch.quantization.get_default_qconfig(backend)
-    quantized_model = torch.quantization.quantize_dynamic(model, qconfig_spec={torch.nn.Linear}, dtype=torch.qint8)
+def quantize_tensor(tensor: Tensor, num_bits=8)->Tensor:
+    symmetric = True
+    max_value = torch.max(tensor)
+    min_value = torch.min(tensor)
+    symmetric = True if (min_value > 0 or max_value < 0) else True
+        
+    if symmetric:
+        r_max = max(abs(min_value), abs(max_value))
+        scale = r_max / (2 ** (num_bits - 1) - 1)
+        quantized_tensor = torch.round(tensor / scale)
+        quantized_tensor = torch.clip(quantized_tensor, -2 ** (num_bits - 1), 2 ** (num_bits - 1) - 1)
+    else: # Asymmetric
+        scale = (max_value - min_value) / (2 ** num_bits - 1)
+        quantized_tensor = torch.round((tensor - min_value) / scale)
+        quantized_tensor = torch.clip(quantized_tensor, 0, 2 ** num_bits - 1)
+    print(torch.min(quantized_tensor*scale)-min_value)   
+    print(torch.max(quantized_tensor*scale)-max_value)    
+    print(torch.mean(quantized_tensor*scale)-tensor.mean())
+    return quantized_tensor, scale
+
+
+def asymmetric_quantize_tensor(tensor, min_value, max_value, num_bits=8):
+    # Compute the scale factor based on the given min and max values
+    scale = (max_value - min_value) / (2 ** num_bits - 1)
+    clipped_tensor = torch.clamp(tensor, min_value, max_value)
+    quantized_tensor = torch.round((clipped_tensor - min_value) / scale)
+    quantized_tensor = quantized_tensor * scale + min_value
+    return quantized_tensor
+
+def quantize_model(model, num_bits=8):
+    quantized_model = model
+    for name, param in quantized_model.named_parameters():
+        print(name)
+        if 'weight' in name:  # Quantize weights
+            quantized_weight, scale = quantize_tensor(param.data.cpu(), num_bits)
+            param.data = quantized_weight.to(param.device)
+        if 'bias' in name:  # Don't quantize biases
+            continue
     return quantized_model
